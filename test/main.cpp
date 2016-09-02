bf8a6738919636771c444308cc7430edc6cfb6f4
Gal Egozi <gegozi@comcast.net>
2016-09-02 03:57:08 -0500

    Initial Commit

---
+++ test/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  test
+//
+//  Created by Gal Egozi 1 on 9/2/16.
+//  Copyright © 2016 Gal Egozi. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
